5a2eb32086e1916d4c552b462f5ce1bda544c594
William O'Brien <wobrien2@uw.edu>
2020-11-17 19:35:10 -0800

    Lesson08 assignment

--- blogging/views.py
+++ blogging/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import loader
+# from django.shortcuts import render
+from django.http import HttpResponse  # , HttpResponseRedirect, Http404
+# from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
 def stub_view(request, *args, **kwargs):
@@ -18,18 +20,28 @@ def stub_view(request, *args, **kwargs):
     return HttpResponse(body, content_type="text/plain")
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
+class BlogListView(ListView):
+    queryset = Post.objects.order_by('-published_date').exclude(published_date__exact=None)
+    template_name = 'blogging/list.html'
 
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+
+
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     posts = published.order_by('-published_date')
+#     context = {'posts': posts}
+#     return render(request, 'blogging/list.html', context)
+
+
+# def detail_view(request, post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     return render(request, 'blogging/detail.html', context)
