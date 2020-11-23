cf5d46aad1fba81792ffa116cffc81abeb42d407
William O'Brien <wobrien2@uw.edu>
2020-11-22 16:47:44 -0800

    Model Form Second Try

---
+++ blogging/forms.py
@@ -0,0 +1,10 @@
+# blogging/forms.py
+
+from django.forms import ModelForm
+from blogging.models import Post
+
+
+class MyPostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "text", "author"]
