f087dd7dacf021be80484462a1191c6200e5dbb4
William O'Brien <wobrien2@uw.edu>
2020-11-22 21:28:19 -0800

    Committing add on 1

---
+++ blogging/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from blogging.models import Post, Category
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # Fill me
+    exclude = ("posts",)
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ blogging/views.py
@@ -0,0 +1,15 @@
+from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+
+class BlogListView(ListView):
+    queryset = Post.objects.order_by("-published_date").exclude(
+        published_date__exact=None
+    )
+    template_name = "blogging/list.html"
+
+
+class BlogDetailView(DetailView):
+    queryset = Post.objects.exclude(published_date__exact=None)
+    template_name = "blogging/detail.html"
